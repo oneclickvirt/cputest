c184a3d11f00c12876745346725fe7343134daca
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-07-17 14:07:09 +0800

    fix

--- cpu/cpu_benchmark_32.go
+++ cpu/cpu_benchmark_32.go
@@ -1,4 +1,4 @@
-//go:build (386 || arm) && !((freebsd || openbsd || netbsd || darwin) && (386 || arm)) && !(windows && arm)
+//go:build (386 || arm) && !((freebsd || openbsd || netbsd || darwin) && (386 || arm)) && !(windows && arm) && !(linux && arm)
 
 package cpu
 
