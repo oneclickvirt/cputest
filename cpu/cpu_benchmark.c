6f26194cb12aed3f6b402d38cee7d2eb75d1ff41
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-07-18 12:15:28 +0000

    fix: 修复C代码忽略

--- cpu/cpu_benchmark.c
+++ cpu/cpu_benchmark.c
@@ -1,4 +1,4 @@
-//go:build ignore ppc64 || mips || mipsle || s390x || (windows && (arm || arm64 || amd64 || 386)) || ((freebsd || openbsd || netbsd || darwin) && (386 || amd64 || arm || arm64)) || (linux && arm)
+// +build ignore ppc64 || mips || mipsle || s390x || (windows && (arm || arm64 || amd64 || 386)) || ((freebsd || openbsd || netbsd || darwin) && (386 || amd64 || arm || arm64)) || (linux && arm)
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
